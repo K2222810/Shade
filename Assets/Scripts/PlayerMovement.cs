4a7b9e309a3b3859056a3fe0e36b7bdd6445293c
DysaJC <k2225064@kingston.ac.uk>
2025-07-14 15:56:49 +0100

    Input System
    The Input is working there are few error when you press w,a,s,d but thats normal also the player cant move or jump yet

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using Mono.Cecil.Cil;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,49 +11,90 @@ public class PlayerMovement : MonoBehaviour
     public float jumpForce = 10f;
     public Transform groundCheck;
     public float groundCheckRadius = 0.2f;
-    public LayerMask groundLayer;
 
-    private Rigidbody2D rb;
-    private Vector2 moveInput;
-    private bool isGrounded;
-    private bool jumpPressed;
+/*    public LayerMask groundLayer;
+    private Vector2 moveInput;*/
+    [Header("INPUT AND MORE")]
+    public CharacterController characterController;
+    //PlAYER INPUT
+    PlayerInput playerInput;
+    //ANIMATION
+    Animator animator;
+
+    //-------------------------------------/////
+    //ONPRESSED
+    private bool isjumpPressed;
+    private bool iswalkingPressed;
+
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
+        playerInput = new PlayerInput();
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+        // PLAYER WALK INPUT SYSTEM 
+        playerInput.PlayerController.Walk.started += OnWalk;
+        playerInput.PlayerController.Walk.canceled += OnWalk;
+        playerInput.PlayerController.Walk.performed += OnWalk;
+        // PLAYER JUMP INPUT SYSTME
+        playerInput.PlayerController.Jump.started += OnJump;
+        playerInput.PlayerController.Jump.canceled += OnJump;
+    }
+    private void OnEnable()
+    {
+        playerInput.PlayerController.Enable();
+    }
+    void OnDisable()
+    {
+        playerInput.PlayerController.Disable();
     }
 
     private void Update()
     {
         // Check ground status
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+/*        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);*/
 
-        // Handle jumping
+/*        // Handle jumping
         if (jumpPressed && isGrounded)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-            jumpPressed = false;
-        }
+*//*            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+*//*            jumpPressed = false;
+        }*/
     }
-
-    private void FixedUpdate()
+    void OnWalk(InputAction.CallbackContext context)
+    {
+        Vector2 input = context.ReadValue<Vector2>();
+        if (input.y > 0)
+        Debug.Log("W pressed (up)");
+        if (input.y < 0)
+        Debug.Log("s pressed (back");
+        if (input.x < 0)
+        Debug.Log("a pressed (left)");
+        if (input.x > 0)
+        Debug.Log("d pressed (right");
+        
+    }
+    void OnJump(InputAction.CallbackContext contex)
+    {
+        Debug.Log("Pressing jump");
+    }
+/*    private void FixedUpdate()
     {
         // Move the player horizontally
-        rb.velocity = new Vector2(moveInput.x * moveSpeed, rb.velocity.y);
-    }
+*//*        rb.velocity = new Vector2(moveInput.x * moveSpeed, rb.velocity.y);
+*//*    }
+*/
 
-    // Called from Input System when move input is updated
-    public void OnMove(InputAction.CallbackContext context)
+/*    public void OnJump(InputAction.CallbackContext context)
     {
-        moveInput = context.ReadValue<Vector2>();
-    }
-
-    // Called from Input System when jump input is pressed
-    public void OnJump(InputAction.CallbackContext context)
-    {
+        Debug.Log("Pressing jump");
         if (context.started)
         {
             jumpPressed = true;
         }
-    }
+        if (context.canceled)
+        {
+            jumpPressed = false;
+        }
+    }*/
 }
